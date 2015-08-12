7c2bfc7425348de53ba51b18c89606b174e34382
Michal Buszkiewicz <embed1@gmail.com>
2015-08-12 14:50:08 +0200

    refactors the tests a bit

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -16,37 +16,35 @@ RSpec.describe Appointment, type: :model do
                                           begin_time: "12:30",
                                           end_time: "14:30" }
 
+  let(:appointment) { FactoryGirl.build :appointment,
+                                        doctor: availability.doctor }
+
+  subject { appointment }
+
   context "created with past date" do
-    subject { FactoryGirl.build :appointment,
-                                doctor: availability.doctor,
-                                begins_at: "7 Dec 2015 13:00 +0100",
-                                ends_at: "7 Dec 2015 13:30 +0100" }
+    before { appointment.begins_at = "7 Dec 2015 13:00 +0100"
+             appointment.ends_at = "7 Dec 2015 13:30 +0100" }
     it { should_not be_valid }
   end
 
   context "created with end date before start date" do
-    subject { FactoryGirl.build :appointment,
-                                doctor: availability.doctor,
-                                begins_at: "7 Dec 2015 14:00 +0100",
-                                ends_at: "7 Dec 2015 13:30 +0100" }
+    before { appointment.begins_at = "14 Dec 2015 14:00 +0100"
+             appointment.ends_at = "14 Dec 2015 13:30 +0100" }
     it { should_not be_valid }
   end
 
   context "created so that it matches an availability" do
-    let(:valid_appointment) { FactoryGirl.build :appointment,
-                                doctor: availability.doctor,
-                                begins_at: "14 Dec 2015 13:00 +0100",
-                                ends_at: "14 Dec 2015 13:30 +0100" }
+    before { appointment.begins_at = "14 Dec 2015 13:00 +0100"
+             appointment.ends_at = "14 Dec 2015 13:30 +0100" }
 
     context "without overlapping any other appointment" do
-      subject { valid_appointment }
       it { should be_valid }
     end
 
     context "but overlaps another appointment of the same doctor" do
-      before { valid_appointment.save }
+      before { appointment.save }
       let(:invalid_appointment) { FactoryGirl.build :appointment,
-                                    doctor: valid_appointment.doctor,
+                                    doctor: appointment.doctor,
                                     begins_at: "14 Dec 2015 13:15 +0100",
                                     ends_at: "14 Dec 2015 13:45 +0100" }
       subject { invalid_appointment }
@@ -56,9 +54,9 @@ RSpec.describe Appointment, type: :model do
   end
 
   context "created so that it does not match any availability" do
-    subject { FactoryGirl.build :appointment,
-                                begins_at: "16 Dec 2015 20:00 +0100",
-                                ends_at: "16 Dec 2015 20:30 +0100" }
+    before { appointment.begins_at = "16 Dec 2015 20:00 +0100"
+             appointment.ends_at = "16 Dec 2015 20:30 +0100"
+             appointment.doctor = FactoryGirl.create :user, :doctor }
     it { should_not be_valid }
   end
 
