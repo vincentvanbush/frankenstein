4c255a8877237fbcb10aa565721f18ed003d089a
Michal Buszkiewicz <embed1@gmail.com>
2015-08-12 11:47:47 +0200

    fixes 11-digit pesel number generation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     password "please123"
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    pesel { Random.rand(10**11).to_s }
+    pesel { Random.rand(10**10...10**11).to_s }
     address { Faker::Address.street_address }
 
     trait :admin do
