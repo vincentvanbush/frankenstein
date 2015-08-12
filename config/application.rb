8bca3fd294fb157c2c4cf05b140ea5f8db1562de
Michal Buszkiewicz <embed1@gmail.com>
2015-08-12 12:50:02 +0200

    sets default timezone

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module Frankenstein
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pl
+    config.time_zone = 'Warsaw'
+    config.active_record.default_timezone = 'Warsaw'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
