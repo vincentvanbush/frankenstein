497f8db41a374ff6d5f04b016f78b10b751b7a9e
Michal Buszkiewicz <embed1@gmail.com>
2015-08-18 15:46:59 +0200

    adds uniqueness and association validations to Assignment model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,8 @@
 class Assignment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :clinic
+
+  validates :doctor, presence: true, uniqueness: { scope: :clinic }
+  validates :clinic, presence: true
+
 end
--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -3,4 +3,18 @@ require 'rails_helper'
 RSpec.describe Assignment, type: :model do
   it { should belong_to :doctor }
   it { should belong_to :clinic }
+
+  it { should validate_presence_of :doctor }
+  it { should validate_presence_of :clinic }
+
+  context "when identical to another existing assignment" do
+    # shoulda-matchers validates_uniqueness_of fails strangely...
+    let(:assignment) { FactoryGirl.create :assignment }
+    let(:second_assignment) { FactoryGirl.build :assignment,
+                                doctor: assignment.doctor,
+                                clinic: assignment.clinic }
+    subject { second_assignment }
+    it { should_not be_valid }
+  end
+
 end
