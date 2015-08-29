2a6c28e82bc8a5b02a74c8cfabcfef16cedd9efd
Michal Buszkiewicz <embed1@gmail.com>
2015-08-29 19:49:55 +0000

    adds custom method for pundit authorization error (with 401 status)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:error] = I18n.t 'pundit.user_not_authorized'
+    if user_signed_in?
+      render '/visitors/index', status: 401
+    else
+      render '/devise/sessions/new', status: 401
+    end
+  end
 end
