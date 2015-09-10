5dd2a751dd45d8a06df1d3a611705ccb76d89671
Michal Buszkiewicz <embed1@gmail.com>
2015-09-10 12:19:22 +0000

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,20 @@ Rails.application.routes.draw do
 
   devise_for :users, skip: [:registrations]
   devise_for :admins, skip: [:sessions, :registrations, :passwords]
-  devise_for :patients, skip: [:sessions, :passwords]
-  devise_for :doctors, skip: [:sessions, :passwords]
+  devise_for :patients, skip: [:sessions, :passwords],
+             controllers: { registrations: 'devise_custom/patient_registrations' }
+  devise_for :doctors, skip: [:sessions, :passwords],
+             controllers: { registrations: 'devise_custom/doctor_registrations' }
 
   resources :users, only: [:index]
   resources :doctors, only: [:index, :show, :edit, :update] do
-    resources :assignments, only: [:new, :create, :destroy]
+    # resources :assignments, only: [:new, :create, :destroy]
     resources :availabilities, only: [:new, :create, :destroy]
   end
 
-  resources :appointments, only: [:create, :index, :show, :destroy] do
+  resources :assignments, only: [:index, :create, :destroy]
+
+  resources :appointments, only: [:new, :create, :index, :destroy] do
     member do
       patch :cancel
       patch :confirm
